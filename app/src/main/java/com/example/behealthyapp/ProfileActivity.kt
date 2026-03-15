6f19b37a16a94e6474fbf7e32a4b490575d2bff6
Saif Abd'Essayed <saif.facebook233@gmail.com>
2026-03-15 18:21:46 +0100

    tp4 : Catalogue statique

--- app/src/main/java/com/example/behealthyapp/ProfileActivity.kt
+++ app/src/main/java/com/example/behealthyapp/ProfileActivity.kt
@@ -1,11 +1,21 @@
 package com.example.behealthyapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val btnCatalogue = findViewById<Button>(R.id.btnC)
+
+        btnCatalogue.setOnClickListener {
+            val intent = Intent(this, CatalogueActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
